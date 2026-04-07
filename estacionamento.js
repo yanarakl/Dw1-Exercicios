7e522ad88c8dd7aa4c0a9dc3698d0dec9eaab5f2
Yanara K de Lima <yanarakarczetski@alunos.utfpr.edu.br>
2026-04-07 11:44:46 -0300

    Add files via upload

---
+++ estacionamento.js
@@ -0,0 +1,37 @@
+function CalcularBruto(){
+    debugger
+    let tempo = parseInt(document.getElementById("InputHoras").value);
+   
+    
+    let tempoC = tempo-1;
+    let tempoBruto = (tempoC * 2.5) + 5;
+    
+    document.getElementById("ValorBruto").innerHTML=tempoBruto;
+
+    return tempoBruto
+}
+
+function CalcularTotal(){
+    debugger
+
+    let tempoBruto = CalcularBruto()
+    let tipoVeiculo = document.getElementById("InputVeiculo").checked;
+    let frequencia = document.getElementById("InputFrequencia").checked;
+    let valorPorte;
+    let ValorTotal;
+
+    if(tipoVeiculo){
+        valorPorte = 0.25 * tempoBruto + tempoBruto;
+    }else{
+        valorPorte= tempoBruto;
+    }
+
+    if(frequencia){
+        ValorTotal = valorPorte - (0.05 * valorPorte);
+    }else{
+        ValorTotal= valorPorte;
+    }
+    
+
+    document.getElementById("ValorTotal").innerHTML=ValorTotal.toFixed(2);
+}
