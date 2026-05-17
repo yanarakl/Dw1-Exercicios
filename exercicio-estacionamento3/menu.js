f0713538029c6189f2b60e192c4a136d1c0a18ce
yanarakl <yanarakarczetski@alunos.utfpr.edu.br>
2026-05-17 16:02:26 -0300

    exercicio

--- exercicio-estacionamento3/menu.js
+++
@@ -1,8 +0,0 @@
-if(localStorage.getItem("logado") !== "true") {
-    window.location.href = "login.html";
-}
-
-function sair() {
-    localStorage.removeItem("logado");
-    window.location.href = "login.html";
-}
